9d343a665371e388c3f2f5de81bee6b209a58bae
Diggers33 <cdigby@iris-eng.com>
2025-10-29 14:11:14 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+export default defineConfig({
+  base: '/visummaster/'
+})
+
 
   export default defineConfig({
     plugins: [react()],
